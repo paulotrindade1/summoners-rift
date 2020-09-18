bc4be77d02cdf5374ff54418660fa0c2e5486864
Paulo Trindade <paulo7trindade@gmail.com>
2020-09-18 12:49:32 -0300

    Finished query service

--- blog/query/index.js
+++ blog/query/index.js
@@ -7,12 +7,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {
+const posts = {};
 
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
 app.post('/events', (req, res) => {
+  const { type, data } = req.body;
 
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+
+    console.log('PostCreated!');
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    posts[postId].comments.push({ id, content });
+    
+    console.log('Comment Created!');
+  }
+
+  res.send(posts);
 });
 
 app.listen(4002, () => {
