a8a640e308efa312aa70dd779f665470a442df26
paulotrindade1 <paulo7trindade@gmail.com>
2020-10-04 21:56:48 -0300

    add moderation service

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 export default ({ comments }) => {
   const renderedComments = comments.map(comment => {
-    return (
-      <li key={comment.id}>{comment.content}</li>
-    );
+    let content;
+
+    switch(comment.status) {
+      case 'approved':
+        content = comment.content;
+        break;
+      case 'rejected':
+        content = 'This comment has been rejected';
+        break;
+      case 'pending':
+        content = 'This comment is awaiting moderation';
+        break;
+    }
+
+    return <li key={comment.id}>{content}</li>
   });
 
-  return (
-    <ul>
-      {renderedComments}
-    </ul>
-  );
+  return <ul>{renderedComments}</ul>
 };
